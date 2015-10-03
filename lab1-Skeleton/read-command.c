58768d2a9d7afdb415a0bd86ad8784f05a419244
Simon Ximeng Zou <ximeng@lnxsrv02.seas.ucla.edu>
2015-10-02 23:17:56 -0700

    added some stuff take in characters from input

--- lab1-Skeleton/read-command.c
+++ lab1-Skeleton/read-command.c
@@ -8,6 +8,8 @@
 /* FIXME: You may need to add #include directives, macro definitions,
    static function definitions, etc.  */
 
+#include <stdio.h>
+
 /* FIXME: Define the type 'struct command_stream' here.  This should
    complete the incomplete type declaration in command.h.  */
 
@@ -18,6 +20,15 @@ make_command_stream (int (*get_next_byte) (void *),
   /* FIXME: Replace this with your implementation.  You may need to
      add auxiliary functions and otherwise modify the source code.
      You can also use external functions defined in the GNU C Library.  */
+	printf("make_command_stream\n");
+	char c = get_next_byte(get_next_byte_argument);
+ 	//printf("%c",c);	
+	while (c != EOF)
+ 	{
+		printf("%c",c);
+		c = get_next_byte(get_next_byte_argument);
+	//	printf("%c", c);
+	}
   error (1, 0, "command reading not yet implemented");
   return 0;
 }
